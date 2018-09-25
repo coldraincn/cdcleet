518a06245630f4bff856c92e474915d6691a2d3b
coldraincn <handan213@gmail.com>
2018-09-25 16:54:41 +0800

    完成;删除排序数组中的重复项

--- src/main/java/com/coldraincn/cdcleet/easy/MergeTwoSortedLists.java
+++ src/main/java/com/coldraincn/cdcleet/easy/MergeTwoSortedLists.java
@@ -10,6 +10,26 @@ package com.coldraincn.cdcleet.easy;
  */
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode resultNode=new ListNode(0);
+        ListNode head=resultNode;
+        
+        while(l1!=null&&l2!=null){
+            if(l1.val>l2.val){
+                resultNode.next=l2;
+                l2=l2.next;
+               
+            }else{
+                resultNode.next=l1;
+                l1=l1.next;
+            }
+            resultNode=resultNode.next;
+        }
+        if(l1==null){
+            resultNode.next=l2;
+        }else if(l2==null){
+            resultNode.next=l1;
+        }
+        return head.next;
 
     }
 
@@ -19,6 +39,23 @@ public class MergeTwoSortedLists {
 
         ListNode(int x) {
             val = x;
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("result.val");
+        MergeTwoSortedLists mtsl=new MergeTwoSortedLists();
+        ListNode node1=mtsl.new ListNode(1);
+        node1.next=mtsl.new ListNode(2);
+        node1.next.next=mtsl.new ListNode(4);
+
+        ListNode node2=mtsl.new ListNode(1);
+        node2.next=mtsl.new ListNode(3);
+        node2.next.next=mtsl.new ListNode(4);
+
+        ListNode result=mtsl.mergeTwoLists(node1, node2);
+        while(result!=null){
+            System.out.println(result.val);
+            result=result.next;
         }
     }
 }
