291d70b7309df219ffae2bc1dfd4a08f957fd57d
coldraincn <handan213@gmail.com>
2018-11-20 11:04:21 +0800

    爬楼梯

---
+++ src/main/java/com/coldraincn/cdcleet/easy/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,26 @@
+package com.coldraincn.cdcleet.easy;
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入: 1->1->2
+输出: 1->2
+示例 2:
+
+输入: 1->1->2->3->3
+输出: 1->2->3
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class RemoveDuplicatesfromSortedList{
+    // public ListNode deleteDuplicates(ListNode head) {
+        
+    // }
+}
