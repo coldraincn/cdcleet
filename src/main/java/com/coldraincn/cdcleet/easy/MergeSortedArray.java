cdd64d142e640ce73a82869e944e67ea4cccb983
coldraincn <handan213@gmail.com>
2019-02-18 11:41:17 +0800

    merge sorted array

--- src/main/java/com/coldraincn/cdcleet/easy/MergeSortedArray.java
+++ src/main/java/com/coldraincn/cdcleet/easy/MergeSortedArray.java
@@ -10,44 +10,26 @@ package com.coldraincn.cdcleet.easy;
 示例:
 
 输入:
-nums1 = [1,2,7,0,0,0], m = 3
+nums1 = [1,2,7,0,0,0,0], m = 3
 nums2 = [2,5,6],       n = 3
 
 输出: [1,2,2,3,5,6]
  */
 public class MergeSortedArray{
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;//nums1
-        int j = 0;//nums2
-        while(i<m){
-            if(nums1[i]<=nums2[j]){
-                i++;
-            }else{
-                int temp = nums1[i];
-                nums1[i] = nums2[j];
-                for(int z = 1;z<n;z++){
-                     if(temp>nums2[z]){
-                         nums2[z-1]=nums2[z];
-                     }else{
-                         nums2[z]=temp;
-                         break;
-                     }
-                }
-                
-            }
-        }
-        for(int o = i;o<nums2.length;o++){
-            nums1[o+i] = nums2[o];
-        }
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0)
+            nums1[k--]=nums1[i]>=nums2[j]?nums1[i--]:nums2[j--];
+        while(j>=0) nums1[k--]=nums2[j--];
         
     }
     public static void main(String[] args){
         int[] nums1 = {1,2,7,0,0,0};
         int[] nums2 = {2,5,6};
         MergeSortedArray msa = new MergeSortedArray();
-        msa.merge(nums1,nums1.length,nums2,nums2.length);
-        for(int i = 0;i<nums2.length;i++){
-            System.out.println(nums2[i]);
+        msa.merge(nums1,3,nums2,nums2.length);
+        for(int i = 0;i<nums1.length;i++){
+            System.out.println(nums1[i]);
         }
     }
 }    
--- src/main/java/com/coldraincn/cdcleet/easy/SameTree.java
+++ src/main/java/com/coldraincn/cdcleet/easy/SameTree.java
@@ -1,47 +1,47 @@
-package com.coldraincn.cdcleet.easy;
-/**
- * 给定两个二叉树，编写一个函数来检验它们是否相同。
+// package com.coldraincn.cdcleet.easy;
+// /**
+//  * 给定两个二叉树，编写一个函数来检验它们是否相同。
 
-如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
 
-示例 1:
+// 示例 1:
 
-输入:       1         1
-          / \       / \
-         2   3     2   3
+// 输入:       1         1
+//           / \       / \
+//          2   3     2   3
 
-        [1,2,3],   [1,2,3]
+//         [1,2,3],   [1,2,3]
 
-输出: true
-示例 2:
+// 输出: true
+// 示例 2:
 
-输入:      1          1
-          /           \
-         2             2
+// 输入:      1          1
+//           /           \
+//          2             2
 
-        [1,2],     [1,null,2]
+//         [1,2],     [1,null,2]
 
-输出: false
-示例 3:
+// 输出: false
+// 示例 3:
 
-输入:       1         1
-          / \       / \
-         2   1     1   2
+// 输入:       1         1
+//           / \       / \
+//          2   1     1   2
 
-        [1,2,1],   [1,1,2]
+//         [1,2,1],   [1,1,2]
 
-输出: false
- */
+// 输出: false
+//  */
 
-public class SameTree{
-    // Definition for a binary tree node.
-     public class TreeNode {
-         int val;
-         TreeNode left;
-         TreeNode right;
-         TreeNode(int x) { val = x; }
-    }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+// public class SameTree{
+//     // Definition for a binary tree node.
+//      public class TreeNode {
+//          int val;
+//          TreeNode left;
+//          TreeNode right;
+//          TreeNode(int x) { val = x; }
+//     }
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-    }
-}
+//     }
+// }
