0c287ec97548840d2ac72e9bc6480077c01cbec3
oe <oe@192.168.1.101>
2019-08-13 10:47:09 +0800

    single number

---
+++ src/main/java/com/coldraincn/cdcleet/easy/SingleNumber.java
@@ -0,0 +1,46 @@
+package com.coldraincn.cdcleet.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+
+输入: [2,2,1]
+输出: 1
+示例 2:
+
+输入: [4,1,2,1,2]
+输出: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/single-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SingleNumber{
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int i=0;
+        while(i<nums.length){
+            if(i+2<nums.length&&nums[i]==nums[i+1]){
+                i=i+2;
+            }else{
+                return nums[i];
+            }
+        }
+        return 1;
+        
+    }
+    public static void main(String ...args){
+        int[] a = {4,1,2,1,2};
+        int[] b = {2,2,1,1,3,4,4};
+        int theNumber = new SingleNumber().singleNumber(b);
+        System.out.println(theNumber);
+    }
+}
