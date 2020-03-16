317790f3c09516c0578124e350ea9c821a5a46b4
coldraincn <handan213@gmail.com>
2020-03-16 11:48:30 +0800

    两数相加

--- src/main/java/com/coldraincn/cdcleet/medium/AddTwoNumbers.java
+++ src/main/java/com/coldraincn/cdcleet/medium/AddTwoNumbers.java
@@ -14,19 +14,44 @@ package com.coldraincn.cdcleet.medium;
 原因：342 + 465 = 807
  */
 public class AddTwoNumbers{
-    // public class ListNode{
-    //     int val;
-    //     ListNode next;
-    //     ListNode(int x){
-    //         val = x;
-    //     }
-    // }
-    // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    //     ListNode head ;
-    //     while (l1) {
-            
-    //     }
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+        }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        int sumtemp = 0;
+        ListNode sumnode = new ListNode(-1);
+        ListNode head = sumnode;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + sumtemp;
+            sumnode.next = new ListNode(sum % 10);
+            sumnode = sumnode.next;
+            sumtemp = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
 
-    // }
+        if (sumtemp == 1) {
+            sumnode.next = new ListNode(1);
+        }
+        return head.next;
+
+    }
+  
 
 }
