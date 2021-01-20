585c167b0ad16e5651ce5678210913d38a4863cd
coldraincn <handan213@gmail.com>
2021-01-20 13:52:48 +0800

    threesum

---
+++ src/main/java/com/coldraincn/cdcleet/medium/ThreeSum.java
@@ -0,0 +1,72 @@
+package com.coldraincn.cdcleet.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+ 
+
+示例 1：
+
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+示例 2：
+
+输入：nums = []
+输出：[]
+示例 3：
+
+输入：nums = [0]
+输出：[]
+ 
+
+提示：
+
+0 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+    
+        Arrays.sort(nums);
+  
+        int len = nums.length;
+        for(int i = 0;i < len;++i) {
+            if(nums[i] > 0) return lists;
+
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int curr = nums[i];
+            int L = i+1, R = len-1;
+            while (L < R) {
+                int tmp = curr + nums[L] + nums[R];
+                if(tmp == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(curr);
+                    list.add(nums[L]);
+                    list.add(nums[R]);
+                    lists.add(list);
+                    while(L < R && nums[L+1] == nums[L]) ++L;
+                    while (L < R && nums[R-1] == nums[R]) --R;
+                    ++L;
+                    --R;
+                } else if(tmp < 0) {
+                    ++L;
+                } else {
+                    --R;
+                }
+            }
+        }
+        return lists;
+    }
+}
