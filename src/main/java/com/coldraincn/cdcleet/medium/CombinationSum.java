27f18d46ab6977b392aafc1772fbd73ca6153161
coldraincn <handan213@gmail.com>
2021-06-24 18:22:07 +0800

    CombinationSum

--- src/main/java/com/coldraincn/cdcleet/medium/CombinationSum.java
+++ src/main/java/com/coldraincn/cdcleet/medium/CombinationSum.java
@@ -1,4 +1,8 @@
 package com.coldraincn.cdcleet.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**√
  * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
 
@@ -40,6 +44,28 @@ candidate 中的每个元素都是独一无二的。
  */
 public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
+      List<List<Integer>> ans = new ArrayList<List<Integer>>();
+            List<Integer> combine = new ArrayList<Integer>();
+            dfs(candidates, target, ans, combine, 0);
+            return ans;
     }
-}
+        public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+            if (idx == candidates.length) {
+                return;
+            }
+            if (target == 0) {
+                ans.add(new ArrayList<Integer>(combine));
+                return;
+            }
+            // 直接跳过
+            dfs(candidates, target, ans, combine, idx + 1);
+            // 选择当前数
+            if (target - candidates[idx] >= 0) {
+                combine.add(candidates[idx]);
+                dfs(candidates, target - candidates[idx], ans, combine, idx);
+                combine.remove(combine.size() - 1);
+            }
+        }
+   
+  }
+    
