5be304c165378d8bb70aaf8a3f5d19ae54f38908
coldraincn <handan213@gmail.com>
2021-10-08 13:23:33 +0800

    GroupAnagrams

---
+++ src/main/java/com/coldraincn/cdcleet/medium/GroupAnagrams.java
@@ -0,0 +1,52 @@
+package com.coldraincn.cdcleet.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+
+字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母都恰好只用一次。
+
+ 
+
+示例 1:
+
+输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+示例 2:
+
+输入: strs = [""]
+输出: [[""]]
+示例 3:
+
+输入: strs = ["a"]
+输出: [["a"]]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/group-anagrams
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        var result =  new HashMap<String,List<String>>();
+
+        for(String s:strs){
+            char[] ch=s.toCharArray();
+            Arrays.sort(ch);
+            String key=String.valueOf(ch);
+            if(!result.containsKey(key)) {
+                result.put(key,new ArrayList<>());
+            }   
+              
+            result.get(key).add(s);
+        }
+        return new ArrayList(result.values());
+
+    }
+}
--- src/main/java/com/coldraincn/cdcleet/medium/RotateImage.java
+++ src/main/java/com/coldraincn/cdcleet/medium/RotateImage.java
@@ -41,22 +41,20 @@ matrix[i].length == n
 public class RotateImage {
     public void rotate(int[][] matrix) {
         int len = matrix.length;
-        for(int i = 0;i<len/2;i++){
-            for(int j = 0;j<len/2;j++){
-                var temp = matrix[i][j];
-                matrix[i][j] = matrix[len-i-1][j];
-                matrix[len-i-1][j] = temp;
-            }
-        }
-        for(int i = 0;i<len;i++){
-            for(int j = 0;j<len;j++){
-                if(i == j){
-                    continue;
-                }
-                var temp = matrix[i][j];
+        for(int i = 0;i <= len - 1;++i){
+            for(int j = i+1;j <= len - 1;++j){
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
+        for(int j = 0;j <= (len - 1)/2;++j){
+            for(int i = 0;i <= len - 1;++i){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][len - 1 - j];
+                matrix[i][len - 1 - j] = temp;
+            }
+        }
+        
     }
 }
