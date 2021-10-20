bd4d1221c2bea7db21fd318406ea52420b810039
coldraincn <handan213@gmail.com>
2021-10-20 11:31:23 +0800

    Subsets

--- src/main/java/com/coldraincn/cdcleet/medium/Subsets.java
+++ src/main/java/com/coldraincn/cdcleet/medium/Subsets.java
@@ -1,4 +1,6 @@
 package com.coldraincn.cdcleet.medium;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
 
@@ -12,9 +14,10 @@ package com.coldraincn.cdcleet.medium;
 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 示例 2：
 
-输入：nums = [0]
-输出：[[],[0]]
- 
+输入：nums = [1]
+输出：[[],[1]]
+ 输入：nums = [1,2]
+输出：[[],[1],[2],[1,2]]
 
 提示：
 
@@ -27,7 +30,20 @@ nums 中的所有元素 互不相同
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class  Subsets {
+    
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<>());
+        for(int num:nums){
+            var result_lenth = result.size();
+            List<Integer> subList = new ArrayList<Integer>();
+            for(int i = 0;i<result_lenth;i++){
+                var curList = new ArrayList<>(result.get(i));
+                curList.add(num);
+                result.add(curList);
+            }
+        }
 
+        return result;
     }
 }
