41bf2c76682a5e0ee7453590ca989714e2f49177
coldraincn <handan213@gmail.com>
2021-10-26 11:36:25 +0800

    UniqueBinarySearchTrees

--- src/main/java/com/coldraincn/cdcleet/medium/UniqueBinarySearchTrees.java
+++ src/main/java/com/coldraincn/cdcleet/medium/UniqueBinarySearchTrees.java
@@ -25,6 +25,16 @@ package com.coldraincn.cdcleet.medium;
  */
 public class UniqueBinarySearchTrees {
     public int numTrees(int n) {
+        int [] dp = new int [n+1];
+        dp[0] = 1;
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<=i-1;j++){
+                dp[i]+= dp[j]*dp[i-1-j];
+            }
+        }
+        return dp[n];
+    }
+    private void ubst(int result,int left){
 
     }
 }
