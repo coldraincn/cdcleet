a199401a36ee4480310542c2532977adac09ed77
coldraincn <handan213@gmail.com>
2021-12-10 16:11:55 +0800

    LowestCommonAncestorofaBinaryTree

--- src/main/java/com/coldraincn/cdcleet/medium/Trie.java
+++ src/main/java/com/coldraincn/cdcleet/medium/Trie.java
@@ -39,21 +39,49 @@ insert、search 和 startsWith 调用次数 总计 不超过 3 * 104 次
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Trie {
-    public Trie() {
+    private Trie[] children;
+    private boolean isEnd;
 
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
     }
     
     public void insert(String word) {
-
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
     }
     
     public boolean search(String word) {
-
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
     }
     
     public boolean startsWith(String prefix) {
-
+        return searchPrefix(prefix) != null;
     }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+
 }
 /**
  * Your Trie object will be instantiated and called as such:
