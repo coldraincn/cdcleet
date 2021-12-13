e75329cc3f4fc0b2bd0f15eed09ff862748aecaa
coldraincn <handan213@gmail.com>
2021-12-13 11:01:08 +0800

    ProductofArrayExceptSelf

--- src/main/java/com/coldraincn/cdcleet/medium/ProductofArrayExceptSelf.java
+++ src/main/java/com/coldraincn/cdcleet/medium/ProductofArrayExceptSelf.java
@@ -1,4 +1,7 @@
 package com.coldraincn.cdcleet.medium;
+
+import java.util.Arrays;
+
 /**
  * 给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
 
@@ -23,6 +26,23 @@ package com.coldraincn.cdcleet.medium;
  */
 public class ProductofArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-
+        int len = nums.length;
+        int[] results = new int[len];
+        results[0]=1;
+        for(int i = 1;i<len;i++){
+            results[i] = results[i-1] * nums[i-1];
+        }
+        int right = 1;
+        for(int j = len -1;j>=0;j--){
+            results[j] = results[j] * right;
+            right = nums[j] * right;
+        }
+        return results;
+    }
+    public static void main(String[] args) {
+        var aa = new ProductofArrayExceptSelf();
+        int[] bb = {1,2,3,4};
+        var result = aa.productExceptSelf(bb);
+        System.out.print(Arrays.toString(result));
     }
 }
