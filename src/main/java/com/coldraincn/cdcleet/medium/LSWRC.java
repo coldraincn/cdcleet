3564858949f983711fb5efc611740c097c837e28
coldraincn <handan213@gmail.com>
2022-02-14 13:38:03 +0800

    day6

--- src/main/java/com/coldraincn/cdcleet/medium/LSWRC.java
+++ src/main/java/com/coldraincn/cdcleet/medium/LSWRC.java
@@ -1,4 +1,7 @@
 package com.coldraincn.cdcleet.medium;
+
+import java.util.HashSet;
+
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 
@@ -23,8 +26,28 @@ package com.coldraincn.cdcleet.medium;
 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-// public class LSWRC {
-//     public int lengthOfLongestSubstring(String s){
+public class LSWRC {
+    public int lengthOfLongestSubstring(String s){
 
-//     }
-// }
+         // 哈希集合，记录每个字符是否出现过
+         HashSet<Character> occ = new HashSet<Character>();
+         int n = s.length();
+         // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+         int rk = -1, ans = 0;
+         for (int i = 0; i < n; ++i) {
+             if (i != 0) {
+                 // 左指针向右移动一格，移除一个字符
+                 occ.remove(s.charAt(i - 1));
+             }
+             while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                 // 不断地移动右指针
+                 occ.add(s.charAt(rk + 1));
+                 ++rk;
+             }
+             // 第 i 到 rk 个字符是一个极长的无重复字符子串
+             ans = Math.max(ans, rk - i + 1);
+         }
+         return ans;
+
+    }
+}
